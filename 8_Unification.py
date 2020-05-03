0c92ae69fa2716b2484d65ee107b87f257d4ec4b
sooraju99 <47816525+sooraju99@users.noreply.github.com>
2020-05-03 13:55:38 +0300

    Add files via upload

---
+++ 8_Unification.py
@@ -0,0 +1,95 @@
+def unify(x, y, s):
+    if s is None:
+        return None
+    elif x == y:
+        return s
+    elif is_variable(x):
+        return unify_var(x, y, s)
+    elif is_variable(y):
+        return unify_var(y, x, s)
+    elif isinstance(x, Expr) and isinstance(y, Expr):
+        return unify(x.args, y.args, unify(x.op, y.op, s))
+    elif isinstance(x, str) or isinstance(y, str):
+        return None
+    elif issequence(x) and issequence(y) and len(x) == len(y):
+        if not x: return s
+        return unify(x[1:], y[1:], unify(x[0], y[0], s))
+    else:
+        return None
+
+def is_variable(x):
+    return isinstance(x, Expr) and not x.args and is_var_symbol(x.op)
+
+def unify_var(var, x, s):
+    if var in s:
+        return unify(s[var], x, s)
+    elif occur_check(var, x, s):
+        return None
+    else:
+        return extend(s, var, x)
+
+def occur_check(var, x, s):
+    if var == x:
+        return True
+    elif is_variable(x) and x in s:
+        return occur_check(var, s[x], s)
+    elif isinstance(x, Expr):
+        return (occur_check(var, x.op, s) or
+                occur_check(var, x.args, s))
+    elif isinstance(x, (list, tuple)):
+        return some(lambda element: occur_check(var, element, s), x)
+    else:
+        return False
+
+def extend(s, var, val):
+
+    s2 = s.copy()
+    s2[var] = val
+    return s2
+
+def subst(s, x):
+
+    if isinstance(x, list):
+        return [subst(s, xi) for xi in x]
+    elif isinstance(x, tuple):
+        return tuple([subst(s, xi) for xi in x])
+    elif not isinstance(x, Expr):
+        return x
+    elif is_var_symbol(x.op):
+        return s.get(x, x)
+    else:
+        return Expr(x.op, *[subst(s, arg) for arg in x.args])
+
+def fol_fc_ask(KB, alpha):
+
+    while True:
+        new = {}
+        for r in KB.clauses:
+            ps, q = parse_definite_clause(standardize_variables(r))
+            raise NotImplementedError
+
+def standardize_variables(sentence, dic=None):
+
+    if dic is None: dic = {}
+    if not isinstance(sentence, Expr):
+        return sentence
+    elif is_var_symbol(sentence.op):
+        if sentence in dic:
+            return dic[sentence]
+        else:
+            v = Expr('v_%d' % standardize_variables.counter.next())
+            dic[sentence] = v
+            return v
+    else:
+        return Expr(sentence.op,
+                    *[standardize_variables(a, dic) for a in sentence.args])
+
+standardize_variables.counter = itertools.count()
+
+a = input("Enter first exp: ")
+b = input("Enter second exp: ")
+s = input("Enter substitution: ")
+
+res = unify(a,b,c)
+
+print(res)
